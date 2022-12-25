840a1103ef8c9823b5f6e91a44b45161f2b6aa28
Sourav2707 <smartsourav33@gmail.com>
2022-12-25 20:44:26 +0530

    UC1 creating an address book

--- src/com/day9_address_book/AddressBook.java
+++ src/com/day9_address_book/AddressBook.java
@@ -9,35 +9,17 @@ public class AddressBook {
     String zip;
     String ph_no; //using string for phone number as I have planned to store all these data in an array
     String email;
-    Scanner sc = new Scanner(System.in);
-    String[] contacts = new String[7];
-    public void book() {
-        System.out.println("Enter the contact details");
-        System.out.println("Enter the first name");
-        f_name = "First name: "+sc.next();
-        contacts[0] = f_name;
-        System.out.println("Enter the last name");
-        l_name = "Last name: "+sc.next();
-        contacts[1] = l_name;
-        System.out.println("Enter the address line without spaces");
-        address = "Address: "+sc.next();
-        contacts[2] = address;
-        System.out.println("Enter the state name");
-        state = "State: "+sc.next();
-        contacts[3] = state;
-        System.out.println("Enter the Zip code");
-        zip = "Zip: "+sc.next();
-        contacts[4] = zip;
-        System.out.println("Enter the phone number");
-        ph_no = "Phone number: "+sc.next();
-        contacts[5] = ph_no;
-        System.out.println("Enter the email address");
-        email = "Email id: "+sc.next();
-        contacts[6] = email;
+    String[] contacts = {f_name, l_name, address, state, zip, ph_no, email};
+    public AddressBook(String f_name, String l_name, String address, String state, String zip, String ph_no, String email) {
+        this.f_name = f_name;
+        this.l_name = l_name;
+        this.address = address;
+        this.state = state;
+        this.zip = zip;
+        this.ph_no = ph_no;
+        this.email = email;
     }
-    public void display() {
-        for(int i = 0; i < contacts.length; i++) {
-            System.out.println(contacts[i]);
-        }
+    public String toString() {
+        return f_name+"\n"+l_name+"\n"+address+"\n"+state+"\n"+zip+"\n"+ph_no+"\n"+email;
     }
 }
--- src/com/day9_address_book/Main.java
+++ src/com/day9_address_book/Main.java
@@ -3,10 +3,42 @@ package com.day9_address_book;
 import java.util.Scanner;
 
 public class Main {
+    static String f_name; //first name
+    static String l_name; //last name
+    static String address;
+    static String state;
+    static String zip;
+    static String ph_no; //using string for phone number as I have planned to store all these data in an array
+    static String email;
+    String[] contacts = new String[7];
+    public static void book() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the contact details");
+        System.out.println("Enter the first name");
+        f_name = "First name: "+sc.next();
+        System.out.println("Enter the last name");
+        l_name = "Last name: "+sc.next();
+        System.out.println("Enter the address line without spaces");
+        address  = "Address: "+sc.next();
+        System.out.println("Enter the state name");
+        state = "State: "+sc.next();
+        System.out.println("Enter the Zip code");
+        zip = "Zip: "+sc.next();
+        System.out.println("Enter the phone number");
+        ph_no = "Phone number: "+sc.next();
+        System.out.println("Enter the email address");
+        email = "Email id: "+sc.next();
+    }
+    public void display() {
+        for(int i = 0; i < contacts.length; i++) {
+            System.out.println(contacts[i]);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to day 9 address book program");
-        AddressBook ab = new AddressBook();
-        ab.book();
-        ab.display();
+        Main.book();
+        AddressBook ab = new AddressBook(f_name,l_name,address,state,zip,ph_no,email);
+        System.out.println(ab.toString());
     }
 }
