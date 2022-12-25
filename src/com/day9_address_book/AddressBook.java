e826adf819eab89f6f7d98760f559cb4cd21ee04
Sourav2707 <smartsourav33@gmail.com>
2022-12-25 18:55:02 +0530

    UC1 creating an address book

--- src/com/day9_address_book/AddressBook.java
+++ src/com/day9_address_book/AddressBook.java
@@ -2,6 +2,7 @@ package com.day9_address_book;
 import java.util.Scanner;
 
 public class AddressBook {
+    static int size;
     String f_name; //first name
     String l_name; //last name
     String address;
@@ -14,30 +15,31 @@ public class AddressBook {
     public void book() {
         System.out.println("Enter the contact details");
         System.out.println("Enter the first name");
-        f_name = sc.next();
+        f_name = "First name: "+sc.next();
         contacts[0] = f_name;
         System.out.println("Enter the last name");
-        l_name = sc.next();
+        l_name = "Last name: "+sc.next();
         contacts[1] = l_name;
         System.out.println("Enter the address line without spaces");
-        address = sc.next();
+        address = "Address: "+sc.next();
         contacts[2] = address;
         System.out.println("Enter the state name");
-        state = sc.next();
+        state = "State: "+sc.next();
         contacts[3] = state;
         System.out.println("Enter the Zip code");
-        zip = sc.next();
+        zip = "Zip: "+sc.next();
         contacts[4] = zip;
         System.out.println("Enter the phone number");
-        ph_no = sc.next();
+        ph_no = "Phone number: "+sc.next();
         contacts[5] = ph_no;
         System.out.println("Enter the email address");
-        email = sc.next();
+        email = "Email id: "+sc.next();
         contacts[6] = email;
     }
     public void display() {
-        System.out.println("First name: "+contacts[0]+"\nLast name: "+contacts[1]+"\nAddress: "+contacts[2]+"\nState: "+contacts[3]+
-                "\nZip code: "+contacts[4]+"\nPhone number: "+contacts[5]+"\nEmail address: "+contacts[6]);
+        for(int i = 0; i < contacts.length; i++) {
+            System.out.println(contacts[i]);
+        }
     }
     public static void main(String[] args) {
         System.out.println("Welcome to day 9 address book program");
